c039d62a8cac02646d5a79ce5672dfe7379417eb
Autumn Cummo <autumn.cummo@my.tccd.edu>
2025-09-02 21:31:21 -0500

    Variables and literals

---
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -0,0 +1,15 @@
+/*
+*Your Name
+* Lab #
+* COSC 1436 Fall 2025
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World " << " " << "Your Name" << std::endl;
+    std::cout << "You are " << 20 << " years old" << std::endl;
+std::string name; 
+    name = "Name";
+    std::cout << "Hello World" << name;
+}
