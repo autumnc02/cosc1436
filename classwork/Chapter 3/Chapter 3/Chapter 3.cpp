14be003eb2c3834324dd4aa84f1951442cae3ff7
Autumn Cummo <autumn.cummo@my.tccd.edu>
2025-09-16 20:52:43 -0500

    math

--- classwork/Chapter 3/Chapter 3/Chapter 3.cpp
+++ classwork/Chapter 3/Chapter 3/Chapter 3.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 void main()
+{
+    std::cout << "Enter values for X and Y: ";
+
+    double x, y;
+    std::cin >> x >> y;
+
+    // Function X  Y  Result
+    //------------------------
+    std::cout << "Function " << " X " << " Y " << "Result" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+
+    std::cout << "pow " << x << y << pow(x, y) << std::endl;
+    std::cout << "sqrt " << x << y << sqrt(x) << std::endl;
+
+
+    std::cout << "ceil " << x << y << ceil(x) << std::endl;
+    std::cout << "floor 
+
+
+}
+
+void ExpressionDemo()
 {
     //prompt user fr radius of circle
     std::cout << "Enter radius: ";
@@ -48,5 +71,62 @@ void main()
     largeValue = largeValue - 1;
 
     std::cout << smallValue << " " << largeValue << std::endl;
+
+    //Type coercion - compiler implicitly converts an expression's type to a larger type
+
+    //Type casting - you explicitly convert an expresion's type to another type
+    //c-type casting
+
+    int left = 10;
+    int right = 3;
+
+    double intDivision = 10 / 3; //int
+    intDivision = 10.0 / 3;   // double / int => double
+    intDivision = 10 / 3.0;   // double / int => double
+
+    int intDivision = 10 / 3; //int / int => int
+    intDivision = left / right; // int / int => int
+
+    intDivision = static_cast<double>(left) / right; // double / int => double
+
+    intDivision = (double)left / right; // double / int => double
+
+    //This is wrong and will not produce 5
+    intDivision = (int)"5";
+   // intDivision = static_cast<int>("5"); //Compiler error
+
+    //Math functions
+    //pow(x, y) => power X to the Y power
+    //sqrt(x) => squqare root of X
+    // abs(x) => absolute value of X
+    //
+    //ceil(x) => smallest possible integral value >= X
+    //floor(x) => largest possible integral value <= X
+
+    //round(x) => rounds a float to n int using midpoint rounding
+    // trunc(x) => truncates a float to an int
+
+    // exp(x) => exponential E raises to X power, E = 2.17828
+    // log(x) => logarithm, inverse of exp
+
+    double result;
+    result = pow(5, 3); //5 * 5 * 5 = 125
+    result = pow(125, -3); //cube root of 125
+
+    result = sqrt(100); // 10
+
+    result = abs(-10); //10
+    result = abs(10); //10
+
+    result = ceil(14.5); // 15
+    result = floor(3.1); // 
+
+    result = trunc(14.5); // 14
+    result = trunc(3.1); // 3
+
+    result = exp(4);
+    result = log(exp(4));  // 4
+
+
 
 }
