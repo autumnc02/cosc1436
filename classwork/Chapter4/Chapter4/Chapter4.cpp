07826fdd5cc8f0bc04bc75d3b94e2d8fd1b10c53
Autumn Cummo <autumn.cummo@my.tccd.edu>
2025-09-18 20:43:47 -0500

    ch4

--- classwork/Chapter4/Chapter4/Chapter4.cpp
+++ classwork/Chapter4/Chapter4/Chapter4.cpp
@@ -3,6 +3,7 @@
 * COSC 1436
 */
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -36,7 +37,14 @@ std::cout << "Enter two values: ";
     areEqual = someValue == someOtherValue;
     std::cout << areEqual << std::endl;
 
+    // Comparison works but is case sensitive
     std::string name1 = "Bob";
     std::string name2 = "bob";
     std::cout << (name1 == name2) << std::endl;
+
+    // To compare strings case insensitive use the strcmpi
+    // > 0 ::= left > right
+    // < 0 ::= left < right
+    // == 0 ::= left == right
+    areEqual = strcmpi(name1.c_str(), name2.c_str()) == 0;
 }
