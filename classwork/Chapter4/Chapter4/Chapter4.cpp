b378c2d550a3f4c32d4099dad39050c02b1e6232
Autumn Cummo <autumn.cummo@my.tccd.edu>
2025-09-18 21:36:40 -0500

    ch4

--- classwork/Chapter4/Chapter4/Chapter4.cpp
+++ classwork/Chapter4/Chapter4/Chapter4.cpp
@@ -7,6 +7,58 @@
 
 int main()
 {
+    //Movie details
+    std::string title;                     //Required
+    std::string description;               //Optional
+    int runLength;                         //Required, 0
+    int releaseYear;                       //Optional, but between 1900-2100
+    double userRating;                     //Optional, 1.0-10.0
+    bool isClassic;                        //Required, false
+
+    //Get movie details
+    std::cout << "Enter movie title: ";
+    std::getline(std::cin, title);
+
+    //Title is required
+    bool isEmpty = title == "";
+    if (isEmpty)
+        std::cout << "Title is required" << std::endl;
+
+    std::cout << "Enter the run length (in minuetes): ";
+    std::cin >> runLength;
+    if (runLength < 0)
+    {
+        std::cout << "Run length must be at least 0" << std::endl;
+        runLength = 0;
+    };
+
+    std::cout << "Enter the release year (900-2100): ";
+    std::cin >> releaseYear;
+    if (releaseYear < 1900)
+    {
+        std::cout << "Release year must be at least 1900" << std::endl;
+        releaseYear = 1900;
+    };
+
+
+    std::cout << "Enter the optional description: ";
+    std::getline(std::cin, description);
+
+    std::cout << "Enter the optional user rating (1.0-10.0): ";
+    std::cin >> userRating;
+
+    std::cout << "Is this a classic (Y/N)? ";
+    std::string input;
+    std::cin >> input;
+    if (_strcmpi(input.c_str(), "Y") == 0)
+        isClassic = true
+    if (_strcmpi(input.c_str(), "N") == 0)
+        isClassic = false
+    //TODO: Handle other values
+}
+
+void relationalDemo()
+{
 std::cout << "Enter two values: ";
 
     int left, right;
@@ -46,5 +98,5 @@ std::cout << "Enter two values: ";
     // > 0 ::= left > right
     // < 0 ::= left < right
     // == 0 ::= left == right
-    areEqual = strcmpi(name1.c_str(), name2.c_str()) == 0;
+    areEqual = _strcmpi(name1.c_str(), name2.c_str()) == 0;
 }
