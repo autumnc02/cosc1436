47181ec329018a295ac4552013fbe5e9347d2299
Autumn Cummo <autumn.cummo@my.tccd.edu>
2025-09-23 21:24:33 -0500

    c

--- classwork/Chapter4/Chapter4/Chapter4.cpp
+++ classwork/Chapter4/Chapter4/Chapter4.cpp
@@ -34,22 +34,41 @@ int main()
 
     std::cout << "Enter the release year (1900-2100): ";
     std::cin >> releaseYear;
-    if (releaseYear < 1900)
+   /* if (releaseYear < 1900)
     {
         std::cout << "Release year must be at least 1900" << std::endl;
         releaseYear = 1900;
     } else if (releaseYear > 2100)
     {
         std::cout << "Release year must be between 1900 and 2100" << std::endl;
-        releaseYear = 1900,
+        releaseYear = 1900;
+    }*/
+    if (releaseYear < 1900 || releaseYear > 2100)
+    {
+        std::cout << "Release year must be between 1900 and 2100" << std::endl;
+        releaseYear = 1900;
     }
-
 
     std::cout << "Enter the optional description: ";
     std::getline(std::cin, description);
 
+    //TODO: Validate userRating
     std::cout << "Enter the optional user rating (1.0-10.0): ";
     std::cin >> userRating;
+    if (userRating < 1.0 || userRating > 10.0)
+    {
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
+    }
+    /*if (userRating < 1.0)
+    {
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
+    } else if (userRating > 10.0)
+    {
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 10.0;
+    }*/
 
     std::cout << "Is this a classic (Y/N)? ";
     std::string input;
--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -9,7 +9,7 @@ int main()
 
     std::cout << "Autumn Cummo" << std::endl;
     std::cout << "Lab1" << std::endl;
-    std::cout << "COSC 1436 Fall 2025" << std::endl;
+    std::cout << "COSC 1436 Fall 2025" << std::endl << std::endl;
 
     std::cout << "Enter your name: ";
     
@@ -41,21 +41,39 @@ int main()
     int finalExam;
     std::cout << "Enter final exam: ";
     std::cin >> finalExam;
+    std::cout << std::endl;
 
     std::cout << name << ", your lab grades are: " << std::endl;
     std::cout << "lab 1 = " << lab1 << std::endl;
     std::cout << "Lab 2 = " << lab2 << std::endl;
     std::cout << "Lab 3 = " << lab3 << std::endl;
-    std::cout << "Lab 4 = " << lab4 << std::endl;
+    std::cout << "Lab 4 = " << lab4 << std::endl << std::endl;
 
     std::cout << name << ", your exam grades are: " << std::endl;
     std::cout << "Exam 1 = " << exam1 << std::endl;
     std::cout << "Exam 2 = " << exam2 << std::endl;
-    std::cout << "Exam 3 = " << exam3 << std::endl;
+    std::cout << "Exam 3 = " << exam3 << std::endl << std::endl;
 
     std::cout << name << ", your other grades are: " << std::endl;
     std::cout << "Participation = " << participation << std::endl;
-    std::cout << "Final Exam = " << finalExam << std::endl;
+    std::cout << "Final Exam = " << finalExam << std::endl << std::endl;
+
+
+    std::cout << name << ", your class grade is: " << std::endl;
+    int labAverage = (lab1 + lab2 + lab3 + lab4) / 4;
+    std::cout << "Lab Average (65%) = " << labAverage << std::endl;
+
+    int examAverage = (exam1 + exam2 + exam3) / 3;
+    std::cout << "Exam Average (20%) = " << examAverage << std::endl;
+
+
+    std::cout << "Participation (5%) = ";
+
+
+    std::cout << "Final Exam (10%) = ";
+
+    std::cout <<
+
 
 
 }
