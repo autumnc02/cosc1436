64e3a03110b6a30d67d6cb119c80c19bd41e33f5
Autumn Cummo <autumn.cummo@my.tccd.edu>
2025-09-24 20:01:53 -0500

    finished

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -1,5 +1,5 @@
-/*Autumn Cummo
-* Lab1
+/*Lab 1
+* Autumn Cummo
 * COSC 1436 Fall 2025
 */
 #include <iostream>
@@ -9,14 +9,13 @@
 int main()
 {
 
+    std::cout << "Lab 1" << std::endl;
     std::cout << "Autumn Cummo" << std::endl;
-    std::cout << "Lab1" << std::endl;
     std::cout << "COSC 1436 Fall 2025" << std::endl << std::endl;
 
+    std::string studentName;
     std::cout << "Enter your name: ";
-    
-    std::string name;
-    std::cin >> name;
+    std::cin >> studentName;
 
     int lab1, lab2, lab3, lab4;
     std::cout << "Enter lab 1: ";
@@ -45,29 +44,29 @@ int main()
     std::cin >> finalExam;
     std::cout << std::endl;
 
-    std::cout << name << ", your lab grades are: " << std::endl;
-    std::cout << "lab 1 = " << lab1 << std::endl;
+    std::cout << studentName << ", your lab grades are: " << std::endl;
+    std::cout << "Lab 1 = " << lab1 << std::endl;
     std::cout << "Lab 2 = " << lab2 << std::endl;
     std::cout << "Lab 3 = " << lab3 << std::endl;
     std::cout << "Lab 4 = " << lab4 << std::endl << std::endl;
 
-    std::cout << name << ", your exam grades are: " << std::endl;
+    std::cout << studentName << ", your exam grades are: " << std::endl;
     std::cout << "Exam 1 = " << exam1 << std::endl;
     std::cout << "Exam 2 = " << exam2 << std::endl;
     std::cout << "Exam 3 = " << exam3 << std::endl << std::endl;
 
-    std::cout << name << ", your other grades are: " << std::endl;
+    std::cout << studentName << ", your other grades are: " << std::endl;
     std::cout << "Participation = " << participation << std::endl;
     std::cout << "Final Exam = " << finalExam << std::endl << std::endl;
 
 
-    std::cout << name << ", your class grade is: " << std::endl;
+    std::cout << studentName << ", your class grade is: " << std::endl;
     double labAverage = (lab1 + lab2 + lab3 + lab4) / 4.0;
-    std::cout << std::setprecision(2);
+    std::cout << std::fixed << std::setprecision(2);
     std::cout << "Lab Average (65%) = " << labAverage << "%" << std::endl;
 
     double examAverage = (exam1 + exam2 + exam3) / 3.0;
-    std::cout << std::setprecision(2);
+    std::cout << std::fixed << std::setprecision(2);
     std::cout << "Exam Average (20%) = " << examAverage << "%" << std::endl;
 
     std::cout << "Participation (5%) = " << participation << "%" << std::endl;
@@ -77,7 +76,4 @@ int main()
     double classAverage = (labAverage * 0.65) + (examAverage * 0.20) + (participation * 0.05) + (finalExam * 0.10);
     std::cout << std::fixed << std::setprecision(2);
     std::cout << "Class Average = " << classAverage << "%" << std::endl;
-
-
-
 }
