b4dec49b960e44b08984c868a44afed3b0533107
Autumn Cummo <autumn.cummo@my.tccd.edu>
2025-10-18 13:47:48 -0500

    continue

--- labs/Lab2/interestRates/interestRates.cpp
+++ labs/Lab2/interestRates/interestRates.cpp
@@ -20,7 +20,7 @@ int main()
     std::cin >> loanBalance;
     while (loanBalance < 1 || loanBalance > 1000)
     {
-        std::cout << "That is an invalid value" << std::endl;
+        std::cout << "ERROR: That is an invalid value" << std::endl;
         std::cout << "Please enter the loan amount: ";
         std::cin >> loanBalance;
     }
@@ -30,7 +30,7 @@ int main()
     std::cin >> interestRate;
     while (interestRate < 1.0 || interestRate > 100.0)
     {
-        std::cout << "That is an invalid value" << std::endl;
+        std::cout << "ERROR: That is an invalid value" << std::endl;
         std::cout << "Please enter the interest rate: ";
         std::cin >> interestRate;
     }
@@ -40,7 +40,7 @@ int main()
     std::cin >> payment;
     while (payment < 0 || payment > loanBalance)
     {
-        std::cout << "That is an invalid amount" << std::endl;
+        std::cout << "ERROR: That is an invalid amount" << std::endl;
         std::cout << "How much do you want to pay each month? ";
         std::cin >> payment;
     }
@@ -52,12 +52,15 @@ int main()
               << std::setw(12) << "Interest" << std::setw(12) << "New Balance" << std::endl;
     std::cout << std::setw(70) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
 
-         //interest and new balance
+         //Variables
     double interest = 0.0;
     double newBalance = loanBalance;
+    double totalInterest = 0.0;
+    double totalPayments = 0.0;
 
     for (int month = 1; month <= 12; month++)
     {
+         //Month 1 only
         if (month == 1)
         {
             std::cout << std::setw(10) << std::left << month << "$" << std::setw(12) << loanBalance
@@ -65,29 +68,40 @@ int main()
                       << std::endl;
             continue;
         } 
-        if (loanBalance > 0.0)
+          //Keeps showing 0 when balance = 0
+        if (loanBalance <= 0.0)
         {
-            interest = loanBalance * (interestRate / 100.0);
-        } else if (loanBalance < 0.0)
+            interest = 0.0;
+            newBalance = 0.0;
+        }
+          //Keeps payment from exceeding loan balance
+        if (payment > loanBalance)
         {
             payment = loanBalance;
-            interest = 0.0;
         }
-        if (newBalance > 0.0)
-        {
-            newBalance = (loanBalance - payment) + interest;
-        } else if (newBalance < 0.0)
+          //Interest and new balance formulas
+        interest = (newBalance - payment) * (interestRate / 100.0);
+        newBalance = (loanBalance - payment) + interest;
+
+          //Prevents negative balance
+        if (newBalance < 0.0)
         {
             newBalance = 0.0;
         }
-    
+          //Calculating totals
+        totalInterest += interest;
+        totalPayments += payment;
 
+          //Months 2-12
         std::cout << std::setw(10) << std::left << month << "$" << std::setw(12) << loanBalance << "$" << std::setw(12) << payment
                   << "$" << std::setw(12) << interest << "$" << std::setw(12) << newBalance << std::endl;
 
+         //Updates balance for the next month
         loanBalance = newBalance;
     }
-
-      
+        //Display totals
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << std::setw(10) << std::left << "Total" << std::setw(13) << " " << "$" << std::setw(12) << totalPayments
+              << "$" << std::setw(12) << totalInterest << std::setw(12) << " " << std::endl;
 }
 
